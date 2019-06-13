28c7c43a0d2533f298da119a124d8d147f6ac2be
hunterdunbar <williamhdunbar@gmail.com>
2019-06-13 13:03:22 -0400

    Removing passwords

--- config/default.js
+++ config/default.js
@@ -1,9 +1,9 @@
 module.exports = {
-    SF_URL : process.env.SF_URL || 'https://login.salesforce.com',
-    SF_USERNAME: process.env.SF_USERNAME || 'hdunbar@developerforce.com',
-    SF_PASSWORD: process.env.SF_PASSWORD || 'hunter((19spring',
-    GIT_TOKEN: process.env.GIT_TOKEN || '9d79485d65fc9c713505c5bf24c170f0b1332a73',
-    GIT_OWNER: process.env.GIT_OWNER || 'hunterdunbar',
-    GIT_REPO: process.env.GIT_REPO || 'developer-force-code',
-    GIT_BRANCH: process.env.GIT_BRANCH || 'heads/master'
+    SF_URL : process.env.SF_URL,
+    SF_USERNAME: process.env.SF_USERNAME,
+    SF_PASSWORD: process.env.SF_PASSWORD,
+    GIT_TOKEN: process.env.GIT_TOKEN,
+    GIT_OWNER: process.env.GIT_OWNER,
+    GIT_REPO: process.env.GIT_REPO,
+    GIT_BRANCH: process.env.GIT_BRANCH
 }
